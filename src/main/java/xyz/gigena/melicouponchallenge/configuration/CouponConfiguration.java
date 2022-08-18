92df6fd904c4c1f64890b05be9185394ca88490d
Maximiliano Gigena <maximiliano.@gigena.xyz>
2022-08-18 17:40:37 -0300

    Fixing error with naming

--- src/main/java/xyz/gigena/melicouponchallenge/configuration/CouponConfiguration.java
+++ src/main/java/xyz/gigena/melicouponchallenge/configuration/CouponConfiguration.java
@@ -3,13 +3,13 @@ package xyz.gigena.melicouponchallenge.configuration;
 import java.net.http.HttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import xyz.gigena.melicouponchallenge.service.CouponService;
+import xyz.gigena.melicouponchallenge.service.ItemService;
 
 @Configuration
 public class CouponConfiguration {
   @Bean
-  public CouponService instantiateCouponService() {
+  public ItemService instantiateCouponService() {
     HttpClient client = HttpClient.newHttpClient();
-    return new CouponService(client);
+    return new ItemService(client);
   }
 }
